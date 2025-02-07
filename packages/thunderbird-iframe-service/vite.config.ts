5759aa0613cfe2adf324c1e6cf315ddd9d972e27
Bjørnar Hvidsten <devenania@proton.me>
2025-02-07 02:30:37 +0100

    Remove commented plugin in Vite config

--- packages/thunderbird-iframe-service/vite.config.ts
+++ packages/thunderbird-iframe-service/vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    //plugins: [viteSingleFile()],
     base: "./",
     build: {
         sourcemap: false,
         minify: false,
         rollupOptions: {
             input: {
-                "thunderbird-iframe-server": "thunderbird-iframe-server.html"
-            }
+                "thunderbird-iframe-server": "thunderbird-iframe-server.html",
+            },
         },
         lib: {
             entry: "src/thunderbird-iframe-service.js",
